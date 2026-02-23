099a631ee475d565f3e4d2951da0759e3af86f33
dwoo27 <dwooed27@gmail.com>
2026-02-22 22:12:19 -0800

    10 min commit

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -17,6 +17,15 @@ int main()
 	array<double, RUNNERS> times; //creates array for runners' 100m dash times
 	finTimes(times); //populates array from txt file
 
+	cout << "1. # of times: " << times.size() << endl;
+	cout << "2. Values: " << endl;
+	coutTimes(times); //output all times
+	cout << "3. Front: " << times.front() << endl;
+	cout << "4. Back: " << times.back() << endl;
+	cout << "5. Empty?: " << (times.empty() == 0 ? "False" : "True") << endl;
+	
+
+
 }
 
 void finTimes(array<double, RUNNERS>& times) {
@@ -28,12 +37,17 @@ void finTimes(array<double, RUNNERS>& times) {
 		for (int i = 0; i < RUNNERS; i++) {
 			getline(fin, buf); times[i] = atof(buf.c_str());
 		}
+
 		fin.close();
 	}
 	else {
-		cout << "Could not open file" << endl;
+		cout << "File not found." << endl;
 	}
 
 }
 
-void coutTimes(array<double, RUNNERS>&);
+void coutTimes(array<double, RUNNERS>& times) {
+	for (int i = 0; i < RUNNERS; i++) {
+		cout << i + 1 << ". " << times[i] << endl;
+	}
+}
