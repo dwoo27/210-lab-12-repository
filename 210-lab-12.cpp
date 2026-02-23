d88b8d65c8a024d389a9f5e9b86416aabaf9b746
dwoo27 <dwooed27@gmail.com>
2026-02-22 22:01:20 -0800

    10 min commit

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -2,6 +2,7 @@
 #include <algorithm> //for sort() and find()
 #include <numeric> //for accumulate()
 #include <array>
+#include <string>
 #include <fstream> //for reading file
 using namespace std;
 
@@ -13,14 +14,26 @@ void coutTimes(array<double, RUNNERS>&);
 
 int main()
 {
+	array<double, RUNNERS> times; //creates array for runners' 100m dash times
+	finTimes(times); //populates array from txt file
 
 }
 
-void finTimes(array<double, RUNNERS>&) {
+void finTimes(array<double, RUNNERS>& times) {
 	ifstream fin;
 	fin.open("times.txt");
+	string buf;
 
 	if (fin.good()) {
-
+		for (int i = 0; i < RUNNERS; i++) {
+			getline(fin, buf); times[i] = atof(buf.c_str());
+		}
+		fin.close();
 	}
+	else {
+		cout << "Could not open file" << endl;
+	}
+
 }
+
+void coutTimes(array<double, RUNNERS>&);
